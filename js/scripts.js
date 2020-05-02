73544dd83547a80b9b4d4b5b6b4e2c12b605f03a
Sospeter Kungu <sospeter2030@gmail.com>
2020-05-02 11:31:53 +0300

    did initial commint and added the function to convert gallons to litres and pounds to kilograms

---
+++ js/scripts.js
@@ -0,0 +1,15 @@
+var multiply = function(number1, number2){
+  return number1 * number2;
+}
+var number1 = parseFloat(prompt("Enter the number of litres of your ingredients;"));
+var number2 = 3.78541;
+var number3 = parseFloat(number1 * number2);
+alert("Your conversion of " + number1 + " liters to gallonst is " + number3 + " gallons");
+
+var multiply = function(number1, number2){
+  return number1 * number2;
+}
+var number1 = parseFloat(prompt("Enter the number of pounds of your ingredients;"));
+var number2 = 0.453592;
+var number3 = parseFloat(number1 * number2);
+alert("Your conversion of " + number1 + " pounds to kilograms is " + number3 + " kilograms");
